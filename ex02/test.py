6f00c87c9cb85dbaf853fc2ccfe903dc4bb8e578
Florian Muller <fmuller@student.42.fr>
2020-03-12 17:45:19 +0100

    ex02 done

--- ex02/test.py
+++ ex02/test.py
@@ -4,45 +4,79 @@ from vector import Vector, VectorException
 def test_vector():
     # Init with list
     vec = Vector([1, 2, 3])
-    print(vec.__dict__)
+    print("1. ", vec.__dict__)
 
     # Init with size
     vec = Vector(10)
-    print(vec.__dict__)
+    print("2. ", vec.__dict__)
 
     # Init with range
     vec = Vector((40, 42))
-    print(vec.__dict__)
+    print("3. ", vec.__dict__)
 
     # Add
-    print(Vector(3) + Vector((10, 13)))
+    print("4. ", Vector(3) + Vector((10, 13)))
+
+    # Add scalar
+    print("5. ", Vector([8]) + 2)
+    print("6. ", 2 + Vector([8]))
 
     # Add with diffrent size
     try:
-        print(Vector(3) + Vector(5))
+        print("7. ", Vector(3) + Vector(5))
+        print("7. ~~~ Error ~~~")
     except VectorException as e:
-        print(e)
-    
+        print("7. ", e)
+
     # Sub
-    print(Vector(3) - Vector((10, 13)))
+    print("8. ", Vector(3) - Vector((10, 13)))
+
+    # Sub with scalar
+    print("9. ", Vector([10]) - 5)
+    print("10. ", 5 - Vector([10]))
 
     # Sub with diffrent size
     try:
-        print(Vector(3) - Vector(5))
+        print("11. ", Vector(3) - Vector(5))
+        print("11. ~~~ Error ~~~")
     except VectorException as e:
-        print(e)
+        print("11. ", e)
 
-    # Mul with scalar
-    print(Vector(4) * 4)
+    # Div with scalars
+    print("12. ", Vector([10, 20, 30]) / 2)
+    print("13. ", 12 / Vector([2, 3, 4, 5]))
+
+    # Div with 2 vectors
+    try:
+        print("14. ", Vector(10) / Vector(10))
+        print("14. ~~~ Error ~~~")
+    except VectorException as e:
+        print("14. ", e)
+
+    # Mul with scalars
+    print("15. ", Vector(4) * 4)
+    print("16. ", 4 * Vector(4))
+    print("17. ", Vector(4) * 2.5)
+    print("18. ", 2.5 * Vector(4))
 
     # Mul with Vector
-    print(Vector(4) * Vector(4))
-    
-    # Sub with diffrent size
+    print("19. ", Vector(4) * Vector(4))
+
+    # Mul with diffrent size
     try:
-        print(Vector(3) * Vector(5))
+        print("20. ", Vector(3) * Vector(5))
+        print("20. ~~~ Error ~~~")
     except VectorException as e:
-        print(e)
+        print("20. ", e)
+
+    # str and repr
+    cool_vector = Vector(5)
+
+    print(cool_vector)
+    print(repr(cool_vector))
+
+    # res = eval(repr(cool_vector))
+    # print(res)
 
 
 if __name__ == "__main__":
--- ex02/vector.py
+++ ex02/vector.py
@@ -3,31 +3,6 @@ class VectorException(Exception):
         super().__init__(message)
 
 
-def add_vec(l_vec, r_vec):
-    if not l_vec.size == r_vec.size:
-        raise VectorException("Can't add vector of different size")
-    return Vector([l_nbr + r_nbr for l_nbr, r_nbr in zip(l_vec.values, r_vec.values)])
-
-
-def sub_vec(l_vec, r_vec):
-    if not l_vec.size == r_vec.size:
-        raise VectorException("Can't subtract vector of different size")
-    return Vector([l_nbr - r_nbr for l_nbr, r_nbr in zip(l_vec.values, r_vec.values)])
-
-
-def div_vec(a, b):
-    pass
-
-
-def mul_vec(vec, other):
-    if isinstance(other, int):
-        return Vector([nbr * other for nbr in vec.values])
-    elif isinstance(other, Vector):
-        if not vec.size == other.size:
-            raise VectorException("Can't multiply vector of different size")
-        return sum([l_nbr * r_nbr for l_nbr, r_nbr in zip(vec.values, other.values)])
-
-
 class Vector():
     def __init__(self, init):
         if isinstance(init, list):
@@ -40,32 +15,74 @@ class Vector():
             self.values = [float(nbr) for nbr in range(*init)]
             self.size = len(self.values)
 
+    @staticmethod
+    def __scalar_to_vec(nbr):
+        if isinstance(nbr, int) or isinstance(nbr, float):
+            return Vector([float(nbr)])
+        return nbr
+
+    @staticmethod
+    def __add(l_vec, r_vec):
+        if not l_vec.size == r_vec.size:
+            raise VectorException("Can't add vector of different size")
+        return Vector([l_nbr + r_nbr for l_nbr, r_nbr in
+                      zip(l_vec.values, r_vec.values)])
+
+    @staticmethod
+    def __sub(l_vec, r_vec):
+        if not l_vec.size == r_vec.size:
+            raise VectorException("Can't subtract vector of different size")
+        return Vector([l_nbr - r_nbr for l_nbr, r_nbr in
+                      zip(l_vec.values, r_vec.values)])
+
+    @staticmethod
+    def __div(left, right):
+        if isinstance(left, Vector) and isinstance(right, (int, float)):
+            return Vector([l_nbr / right for l_nbr in left.values])
+        elif isinstance(left, (int, float)) and isinstance(right, Vector):
+            return Vector([left / r_nbr for r_nbr in right.values])
+        else:
+            raise VectorException("You can only divide Vector with scalars")
+
+    @staticmethod
+    def __mul(vec, other):
+        if isinstance(other, (int, float)):
+            return Vector([nbr * other for nbr in vec.values])
+        elif isinstance(other, Vector):
+            if not vec.size == other.size:
+                raise VectorException("Can't multiply vector"
+                                      " of different size")
+            return sum([l_nbr * r_nbr for l_nbr, r_nbr in
+                       zip(vec.values, other.values)])
+
     def __str__(self):
-        return str(self.values)
+        return f"V{str(self.values)}"
 
     def __repr__(self):
-        return self.__dict__
+        return f"Vector({self.values})"
+        # return str(self.__dict__)
 
-    def __add__(self, rs):
-        return add_vec(self, rs)
+    def __add__(self, right_side):
+        return Vector.__add(self, Vector.__scalar_to_vec(right_side))
 
-    def __radd__(self, ls):
-        return add_vec(ls, self)
+    def __radd__(self, left_side):
+        return Vector.__add(Vector.__scalar_to_vec(left_side), self)
 
-    def __sub__(self, rs):
-        return sub_vec(self, rs)
+    def __sub__(self, right_side):
+        return Vector.__sub(self, Vector.__scalar_to_vec(right_side))
 
-    def __rsub__(self, ls):
-        return sub_vec(ls, self)
+    def __rsub__(self, left_side):
+        return Vector.__sub(Vector.__scalar_to_vec(left_side), self)
 
-    def __truediv__(self, rs):
-        return div(self, rs)
+    def __truediv__(self, right_side):
+        return Vector.__div(self, right_side)
 
-    def __rtruediv__(self, ls):
-        return div(ls, self)
+    def __rtruediv__(self, left_side):
+        return Vector.__div(left_side, self)
 
-    def __mul__(self, rs):
-        return mul_vec(self, rs)
+    def __mul__(self, right_side):
+        return Vector.__mul(self, right_side)
 
-    def __rmul__(self, ls):
-        return mul_vec(ls, self)
+    def __rmul__(self, left_side):
+        # No diffrence, so left side to the right
+        return Vector.__mul(self, left_side)
