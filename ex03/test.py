195d4aec48889ec69e6c038262b5181b0c392d9d
Florian Muller <fmuller@student.42.fr>
2020-03-12 20:40:56 +0100

    doing ex03

---
+++ ex03/test.py
@@ -0,0 +1,12 @@
+from matrix import Matrix, MatrixException
+
+
+def test_init_matrix():
+    mat = Matrix([
+        [1, 2],
+        [3, 4]
+    ])
+
+
+if __name__ == "__main__":
+    test_init_matrix()
